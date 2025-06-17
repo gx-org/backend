32e5672bf3f46228848cf2feb0cfcd6b886d7a7e
GX authors <noreply@google.com>
2025-06-17 09:05:02 -0700

    Project import generated by Copybara.
    PiperOrigin-RevId: 772496977

--- graph/graph.go
+++ graph/graph.go
@@ -86,66 +86,66 @@ type (
 		// Graph returns the graph in which the nodes are created into.
 		Graph() Graph
 
-		// NewConstant returns a node representing a numerical constant value in the graph.
-		NewConstant(value platform.HostBuffer) (Node, error)
+		// Constant returns a node representing a numerical constant value in the graph.
+		Constant(value platform.HostBuffer) (Node, error)
 
-		// NewTuple returns a node representing a tuple of nodes.
-		NewTuple(nodes []Node) (Tuple, error)
+		// Tuple returns a node representing a tuple of nodes.
+		Tuple(nodes []Node) (Tuple, error)
 
-		// NewCall returns a node that invokes a subgraph.
-		NewCall(sg Subgraph, args ...Node) (Node, error)
+		// Call returns a node that invokes a subgraph.
+		Call(sg Subgraph, args ...Node) (Node, error)
 
-		// NewSubgraph returns a Graph instance that maps to a new subgraph.
-		NewSubgraph(name string) (Graph, error)
+		// Subgraph returns a Graph instance that maps to a new subgraph.
+		Subgraph(name string) (Graph, error)
 
-		// NewArgument returns a node set by a caller when calling the function.
-		NewArgument(name string, shape *shape.Shape, index int) (Node, error)
+		// Argument returns a node set by a caller when calling the function.
+		Argument(name string, shape *shape.Shape, index int) (Node, error)
 
-		// NewUnary returns a node applying a unary operator to a node.
-		NewUnary(op *ast.UnaryExpr, x Node) (Node, error)
+		// Unary returns a node applying a unary operator to a node.
+		Unary(op *ast.UnaryExpr, x Node) (Node, error)
 
-		// NewBinary returns a node applying a binary operator between two nodes.
-		NewBinary(op *ast.BinaryExpr, x, y Node) (Node, error)
+		// Binary returns a node applying a binary operator between two nodes.
+		Binary(op *ast.BinaryExpr, x, y Node) (Node, error)
 
-		// NewReshape returns a reshape operator node.
-		NewReshape(x Node, axisLengths []int) (Node, error)
+		// Reshape returns a reshape operator node.
+		Reshape(x Node, axisLengths []int) (Node, error)
 
-		// NewConcat concatenates multiple arrays into a single array.
-		NewConcat(axis int, nodes []Node) (Node, error)
+		// Concat concatenates multiple arrays into a single array.
+		Concat(axis int, nodes []Node) (Node, error)
 
-		// NewCast returns a cast/convert operator node.
-		NewCast(x Node, target dtype.DataType) (Node, error)
+		// Cast returns a cast/convert operator node.
+		Cast(x Node, target dtype.DataType) (Node, error)
 
-		// NewSlice returns a slice on a node.
-		NewSlice(x Node, index int) (Node, error)
+		// Slice returns a slice on a node.
+		Slice(x Node, index int) (Node, error)
 
-		// NewSet returns a node to set a slice in an array.
-		NewSet(x, updates, index Node) (Node, error)
+		// Set returns a node to set a slice in an array.
+		Set(x, updates, index Node) (Node, error)
 
-		// NewDotGeneral returns a general dot operator node.
-		NewDotGeneral(x, y Node, batchAxes, reduceAxes [2][]int) (Node, error)
+		// DotGeneral returns a general dot operator node.
+		DotGeneral(x, y Node, batchAxes, reduceAxes [2][]int) (Node, error)
 
-		// NewWhile returns a while loop node.
-		NewWhile(cond, body Subgraph, state Node) (Node, error)
+		// While returns a while loop node.
+		While(cond, body Subgraph, state Node) (Node, error)
 
-		// NewBroadcastInDim broadcasts data across a given set of axis.
-		NewBroadcastInDim(x Node, shape *shape.Shape, broadcastAxes []int) (Node, error)
+		// BroadcastInDim broadcasts data across a given set of axis.
+		BroadcastInDim(x Node, shape *shape.Shape, broadcastAxes []int) (Node, error)
 	}
 
 	// NumBuilder creates node in the graph for functions in the num package from the standard library.
 	NumBuilder interface {
-		// NewIota returns a node filling an array with values from 0 to number of elements-1.
-		NewIota(sh *shape.Shape, iotaAxis int) (Node, error)
+		// Iota returns a node filling an array with values from 0 to number of elements-1.
+		Iota(sh *shape.Shape, iotaAxis int) (Node, error)
 	}
 
 	// MathBuilder creates node in the graph for functions in the max package from the standard library.
 	MathBuilder interface {
-		// NewCos returns the computation for cosine.
-		NewCos(x Node) (Node, error)
-		// NewSin returns the computation for sine.
-		NewSin(x Node) (Node, error)
-		// NewTanh returns the computation for hyperbolic tangent.
-		NewTanh(x Node) (Node, error)
+		// Cos returns the computation for cosine.
+		Cos(x Node) (Node, error)
+		// Sin returns the computation for sine.
+		Sin(x Node) (Node, error)
+		// Tanh returns the computation for hyperbolic tangent.
+		Tanh(x Node) (Node, error)
 	}
 )
 
