aebd59f2e0e424d799478841d30d6be1cab10fe8
GX authors <noreply@google.com>
2026-08-20 07:28:07 -0700

    Project import generated by Copybara.
    PiperOrigin-RevId: 967810487

--- dtypes/dtype.go
+++ dtypes/dtype.go
@@ -20,7 +20,7 @@ import (
 	"reflect"
 	"unsafe"
 
-	"github.com/gomlx/compute/dtypes/bfloat16"
+	"google3/third_party/golang/github_com/gomlx/gopjrt/v/v0/dtypes/bfloat16/bfloat16"
 )
 
 // DType is the type of an atomic value or type of the data stored in an array.
